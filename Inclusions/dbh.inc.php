7acfddb338774c49efe4782134c55052ca23490f
Jake <jakebos1@icloud.com>
2024-05-23 16:36:30 +0200

    Update dbh.inc.php

--- Inclusions/dbh.inc.php
+++ Inclusions/dbh.inc.php
@@ -1,17 +1,39 @@
 <?php 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "VoedselbankMaaskantje";
+$pdo = connect();
 
-try {
-    // create connection
-    $dsn = "mysql:host=$servername;dbname=$dbname;port=3306";
-    $pdo = new PDO($dsn, $username, $password);
-    
-    // set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+function connect() {
+    $servername = "localhost";
+    $username = "root";
+    $password = "password";
+    $dbname = "VoedselbankMaaskantje";
+
+    try {
+        // create connection
+        $dsn = "mysql:host=$servername;dbname=$dbname;port=3306";
+        $pdo = new PDO($dsn, $username, $password);
+        
+        // set the PDO error mode to exception
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage() . "\n";
+        return null;
+    }
+}
+
+function addMedewerker($voornaam, $tussenvoegsels, $achternaam, $rol, $telefoonnummer, $email, $wachtwoord) {
+    global $pdo;
+
+    $hashed_pw = password_hash($wachtwoord, 0);
+
+    // Commit to database
+    $stmt = $pdo->prepare("INSERT INTO Medewerker(Voornaam, Tussenvoegsels, Achternaam, Rol, TelefoonNummer, Email, Wachtwoord) VALUES (?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bindParam(1, $voornaam);
+    $stmt->bindParam(2, $tussenvoegsels);
+    $stmt->bindParam(3, $achternaam);
+    $stmt->bindParam(4, $rol);
+    $stmt->bindParam(5, $telefoonnummer);
+    $stmt->bindParam(6, $email);
+    $stmt->bindParam(7, $hashed_pw);
+    $stmt->execute();
 }
