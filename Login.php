1b510d38b94d212f2a3c516427764def2eac4c73
Lily-ofthevalley <lilithvdplas@gmail.com>
2024-05-28 10:03:38 +0200

    login aangepast
    heb gemaakt dat je met je voor en achternaam moet inloggen

--- Login.php
+++ Login.php
@@ -11,8 +11,9 @@
       <div class="login">
         <h1>Inloggen</h1>
         <form action="Responses/loginResponse.php" method="post">
-            <input type="text" name="username" value="" placeholder="Gebruikersnaam"><br>
-            <input type="password" name="password" value="" placeholder="Wachtwoord"><br>
+            <input type="text" name="voornaam" value="" placeholder="voornaam" required><br>
+            <input type="text" name="achternaam" value="" placeholder="achternaam" required><br>
+            <input type="password" name="password" value="" placeholder="Wachtwoord" required><br>
             <input type="submit" name="submit" value="Verstuur">
         </form>
       </div>
