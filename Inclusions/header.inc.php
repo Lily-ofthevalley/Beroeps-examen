32ac59284e3aaf50d8a950ccb0cc9bfb0726280d
Lily-ofthevalley <lilithvdplas@gmail.com>
2024-05-29 13:35:34 +0200

    user link
    was vergeten de userlink bij elke rol te zetten

--- Inclusions/header.inc.php
+++ Inclusions/header.inc.php
@@ -12,12 +12,14 @@
         echo "<ul class='header'>";
         echo "<li><a href='Vooraad.php'>Vooraad</a></li>";
         echo "<li><a href='Leveranciers.php'>Leveranciers</a></li>";
+        echo "<li class='userbtn'><a href='User.php'>Welkom, " . ($_SESSION["user"]["username"]) . "</a></li>";
         echo "</ul>";
     } else if($_SESSION["user"]["rol"] == "Vrijwilliger"){
         echo "<ul class='header'>";
         echo "<li><a href='Vooraad.php'>Vooraad</a></li>";
         echo "<li><a href='Voedselpakketten.php'>Voedselpakketten</a></li>";
         echo "<li><a href='Klanten.php'>Klanten</a></li>";
+        echo "<li class='userbtn'><a href='User.php'>Welkom, " . ($_SESSION["user"]["username"]) . "</a></li>";
         echo "</ul>";
     }
 
