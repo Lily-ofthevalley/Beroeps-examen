b8a45165fa4a5835af73a6759b2e3f7b7ab58d58
Roy Stam <rstam@roc-dev.com>
2024-06-21 03:52:47 +0200

    Add user page update

--- AddGebruiker.php
+++ AddGebruiker.php
@@ -20,6 +20,8 @@ session_start();
   <header>
     <?php require_once "Inclusions/header.inc.php" ?>
   </header>
+
+  <!-- OLD SCREEN 
   <div class="addGebruikersContainer">
     <div id="terugNaarGebruikers">
       <a href="Gebruikers.php">
@@ -44,6 +46,37 @@ session_start();
         <input type="submit" name="submit" value="Voeg toe">
       </form>
     </div>
+  </div> -->
+
+  <div class="page-content">
+    <form class="form-container" action="Responses/addGebruikerResponse.php" method="POST">
+      <h2 class="profile__password-header">Gebruiker toevoegen</h2>
+      <div class="form">
+        <label class="form__label" for="Voornaam">Voornaam*</label>
+        <input class="form__input" type="text" id="Voornaam" name="Voornaam" maxlength="45" required>
+
+        <label class="form__label" for="Achternaam">Achternaam*</label>
+        <input class="form__input" type="text" id="Achternaam" name="Achternaam" maxlength="90" required>
+
+        <label class="form__label" for="Rol">Rol*</label>
+        <select class="form__input" id="Rol" name="Rol" required>
+          <option value="" hidden disabled selected>(Kies rol)</option>
+          <option value="Administrator">Administrator</option>
+          <option value="Magazijn medewerker">Magazijn medewerker</option>
+          <option value="Vrijwilliger">Vrijwilliger</option>
+        </select>
+
+        <label class="form__label" for="TelefoonNummer">Aantal*</label>
+        <input class="form__input" type="number" id="TelefoonNummer" name="TelefoonNummer" maxlength="15" required>
+
+        <label class="form__label" for="Email">E-mail*</label>
+        <input class="form__input" type="email" id="Email" name="Email" maxlength="15" required>
+
+        <label class="form__label" for="Wachtwoord">Wachtwoord*</label>
+        <input class="form__input" type="text" id="Wachtwoord" name="Wachtwoord" maxlength="64" autocomplete="off" required>
+      </div>
+      <button class="form__submit" type="submit">Voeg toe</button>
+    </form>
   </div>
 </body>
 
